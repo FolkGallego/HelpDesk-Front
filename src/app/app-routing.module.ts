c20d89ddddabaa17cf668fae3dc34f538a334807
FolkGallego <joaogallegopolo@gmail.com>
2021-10-03 14:15:31 -0300

    Rota para componente home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
 import { NavComponent } from './Components/nav/nav.component';
 
 const routes: Routes = [
   {
-    path: '', component: NavComponent
-  }
+    path: '', component: NavComponent, children: [
+      { path: 'home', component: HomeComponent }
 
+    ]
+  }
 ];
 
 @NgModule({
