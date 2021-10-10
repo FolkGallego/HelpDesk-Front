2d809364097095bc4f1acac366985b50ee3ad1d8
FolkGallego <joaogallegopolo@gmail.com>
2021-10-10 11:58:31 -0300

    Serviço de mensagens implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import { HomeComponent } from './Components/home/home.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { TecnicoListComponent } from './Components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './Components/login/login.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -67,6 +68,11 @@ import { LoginComponent } from './Components/login/login.component';
     MatIconModule,
     MatListModule,
     MatCardModule,
+    ToastrModule.forRoot({
+      timeOut: 4000,
+      closeButton: true,
+      progressBar: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
